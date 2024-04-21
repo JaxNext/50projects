9679f828405b7ad306193641ae9e8acb42bb2272
JaxQian <qianjunyinggo@gmail.com>
2024-04-21 17:06:22 +0800

    feature: 增加示例

---
+++ src/pages/expandCard/cards.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cards-box flex">
+    <div
+      v-for="(color, index) in list"
+      :key="index"
+      :class="['card w-[10px] h-[240px] mr-[18px] rounded-[40px] cursor-pointer', { 'w-full': active === index }]"
+      :style="{ background: color }"
+      @click="active = index"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ list: any[] }>()
+const active = defineModel('active', { default: 0 })
+</script>
+
+<style scoped lang="scss">
+.card {
+  transition: width 0.5s ease;
+}
+</style>
--- src/pages/expandCard/index.vue
+++ src/pages/expandCard/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    ExpandCard
-  </div>
+  <h3>Expand Cards</h3>
+  <Cards v-model:active="active" :list="list" />
 </template>
 
 <script setup lang="ts">
+import Cards from './cards.vue'
+import { ref } from 'vue'
 
+const active = ref(0)
+const list = ['orange', 'yellow', 'blue', 'green', 'red']
 </script>
 
 <style scoped lang="scss">
