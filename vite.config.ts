5695b72a9e5b45374c8e6f82b7ce1a621a0ad770
JaxQian <qianjunyinggo@gmail.com>
2024-06-13 09:39:36 +0800

    bugfix: vite built file 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import path from 'path'
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -15,6 +17,18 @@ export default defineConfig({
   plugins: [UnoCSS(), vue()],
   build: {
     outDir: '50pd',
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+          )
+        },
+      }
+    }
   },
   base: './',
 })
